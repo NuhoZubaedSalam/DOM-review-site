25f49ae141471cac1d19b36f8f0d056537ddcf1e
NuhoZubaedSalam <zs.nuho@gmail.com>
2025-07-31 12:35:54 +0200

    final website

--- script.js
+++ script.js
@@ -1,69 +1,130 @@
 // body
 Object.assign(document.body.style, {
-        backgroundColor: "rgb(245 217 9)",
-        paddingRight: "100px",
-        paddingLeft: "100px"
-    }
-)
+  backgroundColor: "rgb(245 217 9)",
+  paddingRight: "100px",
+  paddingLeft: "100px",
+});
 
 // container
-const container = document.createElement('div')
-document.body.appendChild(container)
+const container = document.createElement("div");
+document.body.appendChild(container);
 
 // heading
-const heading = document.createElement("h1")
-heading.innerText = "What do our users say?"
-container.appendChild(heading)
+const heading = document.createElement("h1");
+heading.innerText = "What do our users say?";
+container.appendChild(heading);
 
 Object.assign(heading.style, {
-        color: "black",
-        fontSize: "50px",
-        paddingTop: "50px",
-        margin: "0 auto",
-        width: "fit-content", 
-        paddingBottom: "35px"
-    }
-)
+  color: "black",
+  fontSize: "50px",
+  padding: "50px",
+  margin: "0 auto",
+  width: "fit-content",
+});
 
-heading.classList.add("montserrat-heading")
+heading.classList.add("montserrat");
+heading.style.fontWeight = "800";
 
 // reviewContainer
 const reviewContainer = document.createElement("div");
-container.appendChild(reviewContainer)
+container.appendChild(reviewContainer);
 
 Object.assign(reviewContainer.style, {
-       display: "flex"
-    }
-)
-
+  display: "flex",
+});
+c
 // reviews
-reviews = []
+reviews = [];
 for (let i = 0; i < 3; i++) {
-    reviews[i] = document.createElement('div') 
-    reviewContainer.appendChild(reviews[i])
+  reviews[i] = document.createElement("div");
+  reviewContainer.appendChild(reviews[i]);
+}
+
+// profilePics
+profilePics = [];
+for (let i = 0; i < 3; i++) {
+  const profilePicDiv = document.createElement("div");
+  const profilePicText = document.createElement("div");
+  
+  profilePics[i] = document.createElement("img");
+
+  switch (i) {
+    case 0:
+      fullName = "John Doe";
+      company = "NYPD";
+      jobTitle = "police";
+      gender = "male";
+      break;
+    case 1:
+      fullName = "Angel Smith";
+      company = "MAYO";
+      jobTitle = "doctor";
+      gender = "female";
+      break;
+    case 2:
+      fullName = "Ben Williams";
+      company = "NASA";
+      jobTitle = "astronomer";
+      gender = "male";
+  }
+
+  const userInfo = document.createElement("h4");
+  userInfo.innerText = `${fullName} \n ${company}`;
+
+  userInfo.classList.add("montserrat")
+
+  Object.assign(userInfo.style, {
+    paddingLeft: "15px",
+    margin: "0",
+    color: "#383838"
+  });
+  profilePics[i].src = `https://avatar.iran.liara.run/public/job/${jobTitle}/${gender}`;
+  
+  Object.assign(profilePicDiv.style, {
+    display: "flex",
+    alignItems: "center",
+  })
+
+  
+  profilePicText.appendChild(userInfo);
+
+  profilePicDiv.appendChild(profilePics[i]);
+  profilePicDiv.appendChild(profilePicText);
+  
+  reviews[i].appendChild(profilePicDiv);
+}
+
+for (const profilePic of profilePics) {
+  Object.assign(profilePic.style, {
+    borderRadius: "50%",
+    width: "60px",
+  });
 }
 
 for (const review of reviews) {
-    const reviewTitle = document.createElement("h2")
-    reviewTitle.innerText = "This is my review"
-    
-    const reviewText = document.createElement("p")
-    reviewText.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ullamcorper tristique eros condimentum feugiat. Mauris non velit id ipsum elementum imperdiet. Mauris facilisis dolor lacinia libero egestas,"
+  const reviewTitle = document.createElement("h2");
+  reviewTitle.innerText = "This is my review";
 
-    review.appendChild(reviewTitle)    
-    review.appendChild(reviewText)
+  const reviewText = document.createElement("p");
+  reviewText.innerText = 
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ullamcorper tristique eros condimentum feugiat. Mauris non velit id ipsum elementum imperdiet. Mauris facilisis dolor lacinia libero egestas,";
 
-    Object.assign(review.style, {
-            textAlign: "justify",
-            margin: "10px",
-            backgroundColor: "white",
-            padding: "20px",
-            borderRadius: "15px",
-            boxShadow: "rgba(0, 0, 0, 0.16) 0px 1px 4px, rgb(51, 51, 51) 0px 0px 0px 3px"
-        }
-    )
+  reviewTitle.style.margin = "15px auto";
 
-     
+  reviewTitle.classList.add("montserrat");
+  reviewText.classList.add("montserrat");
+
+  review.appendChild(reviewTitle);
+  review.appendChild(reviewText);
+
+  Object.assign(review.style, {
+    textAlign: "justify",
+    margin: "10px",
+    backgroundColor: "white",
+    padding: "20px",
+    borderRadius: "15px",
+    boxShadow:
+      "rgba(0, 0, 0, 0.16) 0px 1px 4px, rgb(51, 51, 51) 0px 0px 0px 3px",
+  });
+
 }
-
-
